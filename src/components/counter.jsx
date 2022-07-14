19e82f60368b552a419d76a03704c23f2cddad25
Shabalin_Yakov <shab.yakov@ya.ru>
2022-07-14 09:59:16 +0500

    added component countersList and changed the logic

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,52 +1,34 @@
 import React, { useState } from 'react';
+const Counter = (props) => {
+  const [value, setValue] = useState(props.value);
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
-
-  const formatCount = () => {
-    return count === 0 ? 'empty' : count;
+  const formatValue = () => {
+    return value === 0 ? 'empty' : value;
   };
-  const getBadgeClasses = () => {
+  const getBageClasses = () => {
     let classes = 'badge m-2 ';
-    classes += count === 0 ? 'bg-warning' : 'bg-primary';
+    classes += value === 0 ? 'bg-warning' : 'bg-primary';
     return classes;
   };
+
   const handleIncrement = () => {
-    setCount((prevState) => prevState + 1);
+    setValue((prevState) => prevState + 1);
   };
   const handleDecrement = () => {
-    setCount((prevState) => prevState - 1);
-  };
-  const handleTagChange = (id) => {
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-  };
-  const renderTags = () => {
-    return tags.length !== 0
-      ? tags.map((tag) => (
-          <li
-            key={tag}
-            className="btn btn-primary btn-sm m-2"
-            onClick={() => handleTagChange(tag)}
-          >
-            {tag}
-          </li>
-        ))
-      : 'No tags';
+    setValue((prevState) => prevState - 1);
   };
 
   return (
-    <>
-      <ul>{renderTags()}</ul>
-      <span className={getBadgeClasses()}>{formatCount()}</span>
+    <div>
+      <span>{props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
       </button>
       <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
         -
       </button>
-    </>
+    </div>
   );
 };
 
